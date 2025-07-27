b736181a5132835bee4617fa95c6c8d76835a2c8
12-Jangra <65810506+12-Jangra@users.noreply.github.com>
2025-07-27 15:25:20 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,10 @@
+
+
+a1 = input("Enter  First Number : ")
+a2 = input("Enter  Second Number : ")
+print("Addition :" , int(a1) + int(a2))
+print("Subtraction :" , int(a1) - int(a2))
+print("Multiplication :" , int(a1) * int(a2))
+print("Division :" , int(a1) / int(a2))
+
+
---
+++ Tast2.py
@@ -0,0 +1,3 @@
+Fname = input("Enter Your First Name : ")
+Lname = input("Enter your Last Name : ")
+print("Hello, ", str(Fname) + " " + str(Lname) + " ! Welcome to the Python Program")
